20b2b6afd6fca76023dd30c5a6a86e1410ce849d
Malik <contact@mc-r.fr>
2024-09-11 18:30:13 +0200

    Initial Commit

---
+++ MC-R PerformanceTests/MC_R_PerformanceTests.swift
@@ -0,0 +1,17 @@
+//
+//  MC_R_PerformanceTests.swift
+//  MC-R PerformanceTests
+//
+//  Created by Jenny on 11/09/2024.
+//
+
+import Testing
+@testable import MC_R_Performance
+
+struct MC_R_PerformanceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
